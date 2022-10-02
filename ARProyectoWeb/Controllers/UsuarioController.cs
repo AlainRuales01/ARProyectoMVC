ac66524dd98267101fde4625c3d53124cb0ba198
AlainRuales01 <alainmateo01@gmail.com>
2022-10-02 17:30:37 -0500

    Add Users Index View and prototype of Create view

---
+++ ARProyectoWeb/Controllers/UsuarioController.cs
@@ -0,0 +1,38 @@
+﻿using ARProyectoWeb.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ARProyectoWeb.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private DataBaseContext _context;
+
+        public UsuarioController(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Usuario> usuarios = _context.Usuario.ToList();
+            return View(usuarios);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int? usuarioId)
+        {
+            var usuario = _context.Usuario.Find(usuarioId);
+            return View(usuario);
+        }
+
+        public IActionResult Delete(int? usuarioId)
+        {
+            var usuario = _context.Usuario.Find(usuarioId);
+            return View(usuario);
+        }
+    }
+}
