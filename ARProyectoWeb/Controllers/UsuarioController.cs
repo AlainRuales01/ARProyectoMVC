0fb383b5547afc688824ce10320668da501cc8ae
AlainRuales01 <alainmateo01@gmail.com>
2022-10-03 00:01:31 -0500

    Add Validations

--- ARProyectoWeb/Controllers/UsuarioController.cs
+++ ARProyectoWeb/Controllers/UsuarioController.cs
@@ -27,14 +27,23 @@ namespace ARProyectoWeb.Controllers
         [HttpPost]
         public IActionResult Create(Usuario nuevoUsuario)
         {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Error = "Error";
+                return View(nuevoUsuario);
+            }
             _context.Usuario.Add(nuevoUsuario);
             _context.SaveChanges();
-            return View(nuevoUsuario);
+            return RedirectToAction("Index");
         }
 
         public IActionResult Edit(int? usuarioId)
         {
             var usuario = _context.Usuario.Find(usuarioId);
+            if (usuario == null)
+            {
+                return RedirectToAction("Index");
+            }
             return View(usuario);
         }
 
@@ -42,15 +51,19 @@ namespace ARProyectoWeb.Controllers
         public IActionResult Edit(Usuario usuario)
         {
             var usuarioModificar = _context.Usuario.Find(usuario.UsuarioId);
-            if(usuarioModificar != null)
+            if (string.IsNullOrEmpty(usuario.Nombre) || string.IsNullOrEmpty(usuario.Correo))
+            {
+                ViewBag.Error = "Error";
+                return View(usuario);
+            }
+            if (usuarioModificar != null)
             {
                 usuarioModificar.Nombre = usuario.Nombre;
                 usuarioModificar.Correo = usuario.Correo;
                 _context.Entry(usuarioModificar).State = EntityState.Modified;
                 _context.SaveChanges();
-                return RedirectToAction("Index");
             }
-            return View(usuario);
+            return RedirectToAction("Index");
         }
 
         public IActionResult Delete(int? usuarioId)
@@ -60,9 +73,8 @@ namespace ARProyectoWeb.Controllers
             {
                 _context.Usuario.Remove(usuario);
                 _context.SaveChanges();
-                return RedirectToAction("Index");
             }
-            return View(usuario);
+            return RedirectToAction("Index");
         }
     }
 }
